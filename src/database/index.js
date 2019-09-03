5e01fa3c84e8175bdde888b82c7cd1519f4f2f41
Guilherme Welter <guilherme.a.welter@gmail.com>
2019-09-03 07:33:28 -0300

    Configuring postgres database for sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,18 @@
 import Sequelize from 'sequelize';
+import databaseConfig from '../config/database';
+import User from '../app/models/User';
+
+const models = [User];
 
 class Database {
   constructor() {
     this.init();
   }
 
-  init() {}
+  init() {
+    this.connection = new Sequelize(databaseConfig);
+    models.forEach(model => model.init(this.connection));
+  }
 }
 
 export default new Database();
